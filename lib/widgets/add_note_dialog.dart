e82473bad0011d6ca37e7c5980d39c3bedb2e4fb
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-05-30 18:19:57 -0400

    add notes api call

--- lib/widgets/add_note_dialog.dart
+++ lib/widgets/add_note_dialog.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:period_track/models/note.dart';
+import 'package:period_track/services/database_service.dart';
+import 'package:provider/provider.dart';
 
 class AddNoteDialog extends StatefulWidget {
   const AddNoteDialog({Key? key}) : super(key: key);
@@ -8,10 +12,41 @@ class AddNoteDialog extends StatefulWidget {
 }
 
 class _AddNoteDialogState extends State<AddNoteDialog> {
+  final _db = DatabaseService();
+  final _formKey = GlobalKey<FormState>();
   DateTime _selectedDate = DateTime.now();
+  final _titleController = TextEditingController();
+  final _bodyController = TextEditingController();
+  bool _periodStart = false;
+  bool _intimacy = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    // Clean up the controllers when the widget is disposed.
+    _titleController.dispose();
+    _bodyController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    var user = Provider.of<User?>(context);
+
+    submit() async {
+      if (user == null) return;
+
+      await _db.addNote(
+          user.uid,
+          NoteModel(
+              uid: user.uid,
+              date: _selectedDate,
+              title: _titleController.text,
+              body: _bodyController.text,
+              periodStart: _periodStart,
+              intimacy: _intimacy));
+    }
+
     Widget cancelButton = TextButton(
       child: const Text('CANCEL'),
       onPressed: () {
@@ -19,55 +54,73 @@ class _AddNoteDialogState extends State<AddNoteDialog> {
       },
     );
     Widget continueButton = TextButton(
-      onPressed: () {},
+      onPressed: () {
+        submit();
+        Navigator.of(context).pop();
+      },
       child: const Text('ADD'),
     );
 
     return AlertDialog(
       title: const Text("New Note"),
-      content: SizedBox(
-        width: 600,
-        height: 400,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            InputDatePickerFormField(
-              firstDate: DateTime(2000),
-              lastDate: DateTime(2100),
-              fieldLabelText: 'Date',
-              initialDate: _selectedDate,
-              onDateSubmitted: (date) {
-                setState(() {
-                  _selectedDate = date;
-                });
-              },
+      content: SingleChildScrollView(
+        child: SizedBox(
+          width: 600,
+          height: 400,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InputDatePickerFormField(
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                  fieldLabelText: 'Date',
+                  initialDate: _selectedDate,
+                  onDateSubmitted: (date) {
+                    setState(() {
+                      _selectedDate = date;
+                    });
+                  },
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(
+                    labelText: 'Title',
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  minLines: 4,
+                  maxLines: 6,
+                  controller: _bodyController,
+                  decoration: const InputDecoration(
+                    labelText: 'Note',
+                  ),
+                ),
+                CheckboxListTile(
+                    title: const Text('Period Start'),
+                    value: _periodStart,
+                    onChanged: (val) {
+                      setState(() {
+                        _periodStart = val ?? false;
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading),
+                CheckboxListTile(
+                    title: const Text('Intimacy'),
+                    value: _intimacy,
+                    onChanged: (val) {
+                      setState(() {
+                        _intimacy = val ?? false;
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading),
+              ],
             ),
-            const SizedBox(height: 8),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Title',
-              ),
-            ),
-            const SizedBox(height: 8),
-            const TextField(
-              minLines: 4,
-              maxLines: 6,
-              decoration: InputDecoration(
-                labelText: 'Note',
-              ),
-            ),
-            CheckboxListTile(
-                title: const Text('Period Start'),
-                value: false,
-                onChanged: (val) {},
-                controlAffinity: ListTileControlAffinity.leading),
-            CheckboxListTile(
-                title: const Text('Intimacy'),
-                value: false,
-                onChanged: (val) {},
-                controlAffinity: ListTileControlAffinity.leading),
-          ],
+          ),
         ),
       ),
       actions: [
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,11 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:period_track/widgets/calendar_key.dart';
-import 'package:period_track/models/note.dart';
-import 'package:provider/provider.dart';
-import 'package:period_track/services/database_service.dart';
-
-import '../models/preferences.dart';
 import 'calendar.dart';
 
 class Home extends StatelessWidget {
@@ -15,15 +9,17 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: Column(
-        children: const [
-          SizedBox(height: 48),
-          Calendar(),
-          SizedBox(height: 24),
-          Divider(color: Color(0xffFFBB7C)),
-          SizedBox(height: 24),
-          CalendarKey()
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(height: 24),
+            Calendar(),
+            SizedBox(height: 24),
+            Divider(color: Color(0xffFFBB7C)),
+            SizedBox(height: 24),
+            CalendarKey()
+          ],
+        ),
       ),
     );
   }
