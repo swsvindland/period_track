0d15cfb90b4ba759754ebee13d34e74f30adb675
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-06-03 15:20:06 -0400

    add icons to note

--- lib/widgets/note.dart
+++ lib/widgets/note.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:period_track/utils/constants.dart';
 
 class Note extends StatelessWidget {
-  const Note({Key? key, required this.title, required this.body}) : super(key: key);
+  const Note({Key? key, required this.title, required this.body})
+      : super(key: key);
   final String title;
   final String body;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text(title),
-        subtitle: Text(body),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
+        child: ListTile(
+          title: Text(title),
+          subtitle: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(body),
+              const SizedBox(height: 8),
+              Row(
+                children: const [
+                  CircleAvatar(backgroundColor: primaryDarkColor, maxRadius: 4),
+                  SizedBox(width: 8),
+                  CircleAvatar(
+                      backgroundColor: Color(0xffECCDD6), maxRadius: 4),
+                  SizedBox(width: 8),
+                  CircleAvatar(
+                      backgroundColor: Colors.red, maxRadius: 4),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
