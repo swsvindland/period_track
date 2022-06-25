98e602132a53e91f393b337b9fe41dc812a1441b
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-06-25 18:04:45 -0400

    add acutal admob ids

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -1,16 +1,29 @@
 import "package:os_detect/os_detect.dart" as platform;
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class AdHelper {
 
   static String get bannerAdUnitId {
-    if (platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
-    } else if (platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+    if (kDebugMode) {
+      if (Platform.isAndroid) {
+        return 'ca-app-pub-3940256099942544/6300978111';
+      } else if (Platform.isIOS) {
+        return 'ca-app-pub-3940256099942544/2934735716';
+      } else {
+        throw UnsupportedError('Unsupported platform');
+      }
+    }
+
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-7533750599105635/7832792717';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-7533750599105635/2594028033';
     } else {
-      return '';
+      throw UnsupportedError('Unsupported platform');
     }
   }
+
 
   static String get interstitialAdUnitId {
     if (platform.isAndroid) {
