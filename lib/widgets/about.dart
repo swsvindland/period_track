249b82f7407a4dae5ad0cdd6741476a53d5fbc4d
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-12-12 12:01:34 -0500

    bump version

--- lib/widgets/about.dart
+++ lib/widgets/about.dart
@@ -52,7 +52,7 @@ class About extends StatelessWidget {
                 style: const TextStyle(color: text, fontSize: 16),
               ),
               const Text(
-                '1.7.3',
+                '1.7.4',
                 style: TextStyle(color: text, fontSize: 12),
               ),
             ],
